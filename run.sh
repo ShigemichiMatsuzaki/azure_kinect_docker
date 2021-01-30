c2a82b37ce21d2c63266959b73abc698cd060c3a
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2021-01-30 16:07:52 +0900

    [update] Change scripts

--- run.sh
+++ run.sh
@@ -10,8 +10,12 @@ docker run -it \
     --name azure_kinect \
     --net host \
     --privileged \
-    --env CLIENT_IP=192.168.1.55 \
-    --env MASTER_IP=192.168.1.55 \
+    --env CLIENT_IP=127.0.0.1 \
+    --env MASTER_IP=127.0.0.1 \
+    --env ROS_MASTER_URI=${ROS_MASTER_URI} \
+    --env ROS_IP=${ROS_IP} \
     -v "/etc/localtime:/etc/localtime:ro" \
     -v "/$(pwd)/ros_packages/Azure_Kinect_ROS_Driver:/home/catkin_ws/src/Azure_Kinect_ROS_Driver" \
-    azure_kinect:latest
+    azure_kinect:latest \
+    bash -c "/bin/bash /catkin_build.bash && source /home/catkin_ws/devel/setup.bash && roslaunch azure_kinect_ros_driver driver.launch tf_prefix:=azure_kinect_"
+  
